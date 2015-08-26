36208d72a865534bf6eb30534a8ba4ad7f782bd0
Hanns Holger Rutz <contact@sciss.de>
2015-08-26 12:55:10 +0200

    radically simplify artifact location -- make it expr[file], no child tracking which was anyway problematic due to cyclicity. if we need children in the future, we can extend dependency mechanism of evt.Targets

--- expr/src/main/scala/de/sciss/lucre/artifact/ArtifactLocation.scala
+++ expr/src/main/scala/de/sciss/lucre/artifact/ArtifactLocation.scala
@@ -15,74 +15,46 @@ package de.sciss.lucre.artifact
 
 import java.io.File
 
-import de.sciss.lucre.artifact.impl.{ArtifactImpl => Impl}
-import de.sciss.lucre.data
-import de.sciss.lucre.event.Publisher
-import de.sciss.lucre.stm.{Obj, Sys}
-import de.sciss.model
-import de.sciss.serial.{DataInput, Serializer}
+import de.sciss.lucre.artifact
+import de.sciss.lucre.event.Targets
+import de.sciss.lucre.expr
+import de.sciss.lucre.expr.Expr
+import de.sciss.lucre.stm.Sys
+import de.sciss.serial.ImmutableSerializer
 
-object ArtifactLocation extends Obj.Type {
+object ArtifactLocation extends expr.impl.ExprTypeImpl[File, ArtifactLocation] {
+  import artifact.{ArtifactLocation => Repr}
+
   final val typeID = 0x10003
 
-  def tmp[S <: Sys[S]]()(implicit tx: S#Tx): Modifiable[S] = {
+  def tmp[S <: Sys[S]]()(implicit tx: S#Tx): Const[S] = {
     val dir   = File.createTempFile("artifacts", "tmp")
     dir.delete()
     dir.mkdir()
     dir.deleteOnExit()
-    apply(dir)
+    newConst(dir)
+  }
+  
+  implicit def valueSerializer: ImmutableSerializer[File] = ImmutableSerializer.File
+
+  protected def mkConst[S <: Sys[S]](id: S#ID, value: A)(implicit tx: S#Tx): Const[S] =
+    new _Const[S](id, value)
+
+  protected def mkVar[S <: Sys[S]](targets: Targets[S], vr: S#Var[Ex[S]], connect: Boolean)
+                                  (implicit tx: S#Tx): Var[S] = {
+    val res = new _Var[S](targets, vr)
+    if (connect) res.connect()
+    res
   }
 
-  def apply[S <: Sys[S]](init: File)(implicit tx: S#Tx): Modifiable[S] =
-    Impl.newLocation[S](init)
+  private[this] final class _Const[S <: Sys[S]](val id: S#ID, val constValue: A)
+    extends ConstImpl[S] with Repr[S]
 
-  object Modifiable {
-    implicit def serializer[S <: Sys[S]]: Serializer[S#Tx, S#Acc, ArtifactLocation.Modifiable[S]] =
-      Impl.modLocationSerializer
-
-    def read[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): ArtifactLocation.Modifiable[S] =
-      Impl.readModLocation[S](in, access)
-  }
-  trait Modifiable[S <: Sys[S]] extends ArtifactLocation[S] {
-    /** Registers a significant artifact with the system. That is,
-      * stores the artifact, which should have a real resource
-      * association, as belonging to the system.
-      *
-      * @param file   the file to turn into a registered artifact
-      */
-    def add(file: File)(implicit tx: S#Tx): Artifact.Modifiable[S]
-
-    def remove(artifact: Artifact[S])(implicit tx: S#Tx): Unit
-
-    /** Internal method used during copying. Does not fire event. */
-    private[artifact] def addDirect(artifact: Artifact[S])(implicit tx: S#Tx): Unit
-
-    def directory_=(value: File)(implicit tx: S#Tx): Unit
-  }
-
-  sealed trait Update[S <: Sys[S]] {
-    def location: ArtifactLocation[S]
-  }
-  final case class Added[S <: Sys[S]](location: ArtifactLocation[S], idx: Int, artifact: Artifact[S])
-    extends Update[S]
-
-  final case class Removed[S <: Sys[S]](location: ArtifactLocation[S], idx: Int, artifact: Artifact[S])
-    extends Update[S]
-
-  final case class Moved[S <: Sys[S]](location: ArtifactLocation[S], change: model.Change[File]) extends Update[S]
-
-  implicit def serializer[S <: Sys[S]]: Serializer[S#Tx, S#Acc, ArtifactLocation[S]] = Impl.locationSerializer
-
-  def read[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): ArtifactLocation[S] =
-    Impl.readLocation[S](in, access)
-
-  def readIdentifiedObj[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): Obj[S] =
-    Impl.readIdentifiedLocation(in, access)
+  private[this] final class _Var[S <: Sys[S]](val targets: Targets[S], val ref: S#Var[Ex[S]])
+    extends VarImpl[S] with Repr[S]
 }
 /** An artifact location is a directory on an external storage. */
-trait ArtifactLocation[S <: Sys[S]] extends Obj[S] with Publisher[S, ArtifactLocation.Update[S]] {
-  def directory(implicit tx: S#Tx): File
-  def iterator (implicit tx: S#Tx): Iterator[Artifact[S]]
-
-  def modifiableOption: Option[ArtifactLocation.Modifiable[S]]
+trait ArtifactLocation[S <: Sys[S]] extends Expr[S, File] {
+  /** Alias for `value` */
+  def directory(implicit tx: S#Tx): File = value
 }
--- expr/src/main/scala/de/sciss/lucre/artifact/impl/ArtifactImpl.scala
+++ expr/src/main/scala/de/sciss/lucre/artifact/impl/ArtifactImpl.scala
@@ -18,9 +18,8 @@ import java.io.File
 
 import de.sciss.lucre.artifact.Artifact.Modifiable
 import de.sciss.lucre.event.{EventLike, Targets}
-import de.sciss.lucre.expr.List
 import de.sciss.lucre.stm.impl.ObjSerializer
-import de.sciss.lucre.stm.{Elem, Copy, NoSys, Obj, Sys}
+import de.sciss.lucre.stm.{Copy, Elem, NoSys, Obj, Sys}
 import de.sciss.lucre.{event => evt}
 import de.sciss.model.Change
 import de.sciss.serial.{DataInput, DataOutput, Serializer}
@@ -51,7 +50,7 @@ object ArtifactImpl {
                                        (implicit tx: S#Tx): Artifact.Modifiable[S] = {
     val cookie    = in.readShort()
     if (cookie != SER_VERSION) sys.error(s"Version mismatch. Expected $SER_VERSION but found $cookie")
-    val location  = readLocation(in, access)
+    val location  = Location.read(in, access) // readLocation(in, access)
     val _child    = tx.readVar[String](targets.id, in)
     new Impl[S](targets, location, _child)
   }
@@ -70,120 +69,120 @@ object ArtifactImpl {
     def tpe = Artifact
   }
 
-  // ---- location ----
-
-  def newLocation[S <: Sys[S]](init: File)(implicit tx: S#Tx): Location.Modifiable[S] = {
-    val targets   = evt.Targets[S]
-    val directory = tx.newVar(targets.id, init)
-    val artifacts = List.Modifiable[S, Artifact[S]]
-    new LocationImpl[S](targets, directory, artifacts)
-  }
-
-  def readLocation[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): Location[S] =
-    locationSerializer[S].read(in, access)
-
-  def readModLocation[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): Location.Modifiable[S] =
-    modLocationSerializer[S].read(in, access)
-
-  def locationSerializer[S <: Sys[S]]: Serializer[S#Tx, S#Acc, Location[S]] = anyLocSer.asInstanceOf[LocSer[S]]
-
-  def modLocationSerializer[S <: Sys[S]]: Serializer[S#Tx, S#Acc, Location.Modifiable[S]] =
-    anyModLocSer.asInstanceOf[ModLocSer[S]]
-
-  def readIdentifiedLocation[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): Location[S] = {
-    val targets = Targets.read[S](in, access)
-    readLoc(in, access, targets)
-  }
-
-  private val anyLocSer     = new LocSer[NoSys]
-  private val anyModLocSer  = new ModLocSer[NoSys]
-
-  private def readLoc[S <: Sys[S]](in: DataInput, access: S#Acc, targets: evt.Targets[S])
-                                  (implicit tx: S#Tx): Location.Modifiable[S] = {
-    val cookie    = in.readShort()
-    require(cookie == SER_VERSION, s"Version mismatch. Expected $SER_VERSION but found $cookie")
-    val directory = tx.readVar[File](targets.id, in)
-    val artifacts = List.Modifiable.read[S, Artifact[S]](in, access)
-    new LocationImpl[S](targets, directory, artifacts)
-  }
-
-  private final class LocSer[S <: Sys[S]] extends ObjSerializer[S, Location[S]] {
-    def tpe = Location
-  }
-
-  private final class ModLocSer[S <: Sys[S]] extends ObjSerializer[S, Location.Modifiable[S]] {
-    def tpe = Location
-  }
-
-  private final class LocationImpl[S <: Sys[S]](protected val targets: evt.Targets[S],
-                                                _directory: S#Var[File],
-                                                artifacts: List.Modifiable[S, Artifact[S]])
-    extends Location.Modifiable[S]
-    with evt.impl.SingleNode[S, Location.Update[S]] { loc =>
-
-    def tpe: Obj.Type = Location
-
-    override def toString = s"ArtifactLocation$id"
-
-    private[lucre] def copy()(implicit tx: S#Tx, copy: Copy[S]): Elem[S] = {
-      val res = Location(this.directory)
-      copy.provide(this, res)
-      iterator.foreach { child =>
-        res.addDirect(copy(child))
-      }
-      res
-    }
-
-    def iterator(implicit tx: S#Tx): Iterator[Artifact[S]] = artifacts.iterator
-
-    def modifiableOption: Option[Location.Modifiable[S]] = Some(this)
-
-    def directory(implicit tx: S#Tx): File = _directory()
-    def directory_=(value: File)(implicit tx: S#Tx): Unit = {
-      val change = Change(_directory(), value)
-      if (change.isSignificant) {
-        _directory() = value
-        changed.fire(Location.Moved(loc, change))
-      }
-    }
-
-    object changed extends Changed with evt.impl.RootGenerator[S, Location.Update[S]]
-
-    def remove(artifact: Artifact[S])(implicit tx: S#Tx): Unit = {
-      val idx = artifacts.indexOf(artifact)
-      if (!artifacts.remove(artifact)) throw new NoSuchElementException(s"Artifact $artifact was not found in the store")
-      changed.fire(Location.Removed(loc, idx, artifact))
-    }
-
-    private[artifact] def addDirect(artifact: Artifact[S])(implicit tx: S#Tx): Unit =
-      artifacts.addLast(artifact)
-
-    def add(file: File)(implicit tx: S#Tx): Artifact.Modifiable[S] = {
-      val base      = _directory()
-      val child     = Artifact.relativize(base, file)
-      val artifact  = ArtifactImpl.apply(loc, child)
-      val idx       = artifacts.size
-      artifacts.addLast(artifact)
-      changed.fire(Location.Added(loc, idx, artifact))
-      artifact
-    }
-
-    protected def writeData(out: DataOutput): Unit = {
-      out.writeShort(SER_VERSION)
-      _directory.write(out)
-      artifacts .write(out)
-    }
-
-    protected def disposeData()(implicit tx: S#Tx): Unit = {
-      _directory.dispose()
-      artifacts .dispose()
-    }
-  }
+//  // ---- location ----
+//
+//  def newLocation[S <: Sys[S]](init: File)(implicit tx: S#Tx): Location.Modifiable[S] = {
+//    val targets   = evt.Targets[S]
+//    val directory = tx.newVar(targets.id, init)
+//    val artifacts = List.Modifiable[S, Artifact[S]]
+//    new LocationImpl[S](targets, directory, artifacts)
+//  }
+//
+//  def readLocation[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): Location[S] =
+//    locationSerializer[S].read(in, access)
+//
+////  def readModLocation[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): Location.Modifiable[S] =
+////    modLocationSerializer[S].read(in, access)
+//
+//  def locationSerializer[S <: Sys[S]]: Serializer[S#Tx, S#Acc, Location[S]] = anyLocSer.asInstanceOf[LocSer[S]]
+//
+//  def modLocationSerializer[S <: Sys[S]]: Serializer[S#Tx, S#Acc, Location.Modifiable[S]] =
+//    anyModLocSer.asInstanceOf[ModLocSer[S]]
+//
+//  def readIdentifiedLocation[S <: Sys[S]](in: DataInput, access: S#Acc)(implicit tx: S#Tx): Location[S] = {
+//    val targets = Targets.read[S](in, access)
+//    readLoc(in, access, targets)
+//  }
+//
+//  private val anyLocSer     = new LocSer[NoSys]
+//  private val anyModLocSer  = new ModLocSer[NoSys]
+//
+//  private def readLoc[S <: Sys[S]](in: DataInput, access: S#Acc, targets: evt.Targets[S])
+//                                  (implicit tx: S#Tx): Location.Modifiable[S] = {
+//    val cookie    = in.readShort()
+//    require(cookie == SER_VERSION, s"Version mismatch. Expected $SER_VERSION but found $cookie")
+//    val directory = tx.readVar[File](targets.id, in)
+//    val artifacts = List.Modifiable.read[S, Artifact[S]](in, access)
+//    new LocationImpl[S](targets, directory, artifacts)
+//  }
+//
+//  private final class LocSer[S <: Sys[S]] extends ObjSerializer[S, Location[S]] {
+//    def tpe = Location
+//  }
+//
+//  private final class ModLocSer[S <: Sys[S]] extends ObjSerializer[S, Location.Modifiable[S]] {
+//    def tpe = Location
+//  }
+//
+//  private final class LocationImpl[S <: Sys[S]](protected val targets: evt.Targets[S],
+//                                                _directory: S#Var[File],
+//                                                artifacts: List.Modifiable[S, Artifact[S]])
+//    extends Location.Modifiable[S]
+//    with evt.impl.SingleNode[S, Change[File]] { loc =>
+//
+//    def tpe: Obj.Type = Location
+//
+//    override def toString = s"ArtifactLocation$id"
+//
+//    private[lucre] def copy()(implicit tx: S#Tx, copy: Copy[S]): Elem[S] = {
+//      val res = Location(this.directory)
+//      copy.provide(this, res)
+//      iterator.foreach { child =>
+//        res.addDirect(copy(child))
+//      }
+//      res
+//    }
+//
+//    def iterator(implicit tx: S#Tx): Iterator[Artifact[S]] = artifacts.iterator
+//
+//    def modifiableOption: Option[Location.Modifiable[S]] = Some(this)
+//
+//    def directory(implicit tx: S#Tx): File = _directory()
+//    def directory_=(value: File)(implicit tx: S#Tx): Unit = {
+//      val change = Change(_directory(), value)
+//      if (change.isSignificant) {
+//        _directory() = value
+//        changed.fire(Location.Moved(loc, change))
+//      }
+//    }
+//
+//    object changed extends Changed with evt.impl.RootGenerator[S, Change[File]]
+//
+//    def remove(artifact: Artifact[S])(implicit tx: S#Tx): Unit = {
+//      val idx = artifacts.indexOf(artifact)
+//      if (!artifacts.remove(artifact)) throw new NoSuchElementException(s"Artifact $artifact was not found in the store")
+//      changed.fire(Location.Removed(loc, idx, artifact))
+//    }
+//
+//    private[artifact] def addDirect(artifact: Artifact[S])(implicit tx: S#Tx): Unit =
+//      artifacts.addLast(artifact)
+//
+//    def add(file: File)(implicit tx: S#Tx): Artifact.Modifiable[S] = {
+//      val base      = _directory()
+//      val child     = Artifact.relativize(base, file)
+//      val artifact  = ArtifactImpl.apply(loc, child)
+//      val idx       = artifacts.size
+//      artifacts.addLast(artifact)
+//      changed.fire(Location.Added(loc, idx, artifact))
+//      artifact
+//    }
+//
+//    protected def writeData(out: DataOutput): Unit = {
+//      out.writeShort(SER_VERSION)
+//      _directory.write(out)
+//      artifacts .write(out)
+//    }
+//
+//    protected def disposeData()(implicit tx: S#Tx): Unit = {
+//      _directory.dispose()
+//      artifacts .dispose()
+//    }
+//  }
 
   private final class Impl[S <: Sys[S]](protected val targets: evt.Targets[S],
                                         val location: Location[S], _child: S#Var[String])
     extends Artifact.Modifiable[S]
-    with evt.impl.MappingNode[S, Change[File], Location.Update[S]]
+    with evt.impl.MappingNode[S, Change[File], Change[File]]
     with evt.impl.SingleNode[S, Change[File]] {
 
     def tpe: Obj.Type = Artifact
@@ -201,7 +200,7 @@ object ArtifactImpl {
       val oldP  = _child()
       val newP  = value.path
       if (oldP != newP) {
-        val base    = location.directory
+        val base    = location.value // directory
         _child()    = newP
         val change  = Change(new File(base, oldP), new File(base, newP))
         changed.fire(change)
@@ -210,13 +209,14 @@ object ArtifactImpl {
 
     object changed extends Changed with Mapped
 
-    protected def inputEvent: EventLike[S, Location.Update[S]] = location.changed
+    protected def inputEvent: EventLike[S, Change[File]] = location.changed
 
     protected def foldUpdate(generated: Option[Change[File]],
-                             input: Location.Update[S])(implicit tx: S#Tx): Option[Change[File]] =
+                             input: Change[File])(implicit tx: S#Tx): Option[Change[File]] =
       generated.orElse {
         input match {
-          case Location.Moved(_, Change(oldBase, newBase)) =>
+          case Change(oldBase, newBase) =>
+          // case Location.Moved(_, Change(oldBase, newBase)) =>
             val path    = _child()
             val change  = Change(new File(oldBase, path), new File(newBase, path))
             Some(change)
@@ -225,7 +225,7 @@ object ArtifactImpl {
       }
 
     def value(implicit tx: S#Tx): Artifact.Value = {
-      val base   = location.directory
+      val base   = location.value // directory
       val child  = _child()
       new File(base, child)
     }
