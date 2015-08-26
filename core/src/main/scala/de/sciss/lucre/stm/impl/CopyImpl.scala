d53b2629cb393bff9eb937d45e35ff3cab4d72f7
Hanns Holger Rutz <contact@sciss.de>
2015-08-26 10:28:43 +0200

    like dis

--- core/src/main/scala/de/sciss/lucre/stm/impl/CopyImpl.scala
+++ core/src/main/scala/de/sciss/lucre/stm/impl/CopyImpl.scala
@@ -21,62 +21,49 @@ final class CopyImpl[S <: Sys[S]](implicit tx: S#Tx) extends Copy[S] {
   private[this] final case class Done(elem: Elem[S]) extends State
   private[this] case object Busy extends State
 
-  private[this] val idMap         = tx.newInMemoryIDMap[State]
-  private[this] val constMap      = mutable.Map.empty[Elem[S], State]
-  private[this] val idHintMap     = tx.newInMemoryIDMap[mutable.Map[String, Any]]
-  private[this] val constHintMap  = mutable.Map.empty[Elem[S], mutable.Map[String, Any]]
+  private[this] val stateMap = mutable.Map.empty[Elem[S], State]
+  private[this] val hintMap  = mutable.Map.empty[Elem[S], mutable.Map[String, Any]]
 
   def provide[Repr <: Obj[S]](in: Repr, out: Repr): Unit = {
-    val id = in.id
-    if (!idMap.get(id).contains(Busy))
+    if (!stateMap.get(in).contains(Busy))
       throw new IllegalStateException(s"Copy.provide must be called during copy process: $in")
-    idMap.put(id, Done(out))
+    stateMap.put(in, Done(out))
   }
 
-  private def perform[Repr <: Elem[S]](in: Repr, state: Option[State], put: State => Unit): Repr = state match {
-    case Some(Done(out)) => out.asInstanceOf[Repr]
-    case Some(Busy) => throw new IllegalStateException(s"Cyclic object graph involving $in")
-    case None =>
-      put(Busy)
-      val out = in.copy()(tx, this)
-      put(Done(out))
-      out.asInstanceOf[Repr]
-  }
+  private def perform[Repr <: Elem[S]](in: Repr): Repr =
+    stateMap.get(in) match {
+      case Some(Done(out)) => out.asInstanceOf[Repr]
+      case Some(Busy) => throw new IllegalStateException(s"Cyclic object graph involving $in")
+      case None =>
+        stateMap.put(in, Busy)
+        val out = in.copy()(tx, this)
+        stateMap.put(in, Done(out))
+        out.asInstanceOf[Repr]
+    }
 
   def putHint[Repr <: Elem[S]](in: Repr, key: String, value: Any): Unit = {
-    val map = in match {
-      case obj: Identifiable[_] =>
-        val id = obj.id.asInstanceOf[S#ID]
-        idHintMap.getOrElse(id, { val res = mutable.Map.empty[String, Any]; idHintMap.put(id, res); res })
-      case _ =>
-        constHintMap.getOrElse(in, { val res = mutable.Map.empty[String, Any]; constHintMap.put(in, res); res })
-    }
+    val map = hintMap.getOrElse(in, {
+      val res = mutable.Map.empty[String, Any]
+      hintMap.put(in, res)
+      res
+    })
     map.put(key, value)
   }
 
-  def getHint[Repr <: Elem[S]](in: Repr, key: String): Option[Any] = in match {
-    case obj: Identifiable[_] =>
-      val id = obj.id.asInstanceOf[S#ID]
-      idHintMap.get(id).flatMap(_.get(key))
-    case _ =>
-      constHintMap.get(in).flatMap(_.get(key))
-  }
+  def getHint[Repr <: Elem[S]](in: Repr, key: String): Option[Any] =
+    hintMap.get(in).flatMap(_.get(key))
 
-  def apply[Repr <: Elem[S]](in: Repr): Repr = in match {
-    case obj: Obj[S] =>
-      val id      = obj.id
-      val res     = perform(in, idMap.get(id), idMap.put(id, _))
-      // now copy the attributes
-      val inAttr  = obj.attr
-      val outAttr = res.asInstanceOf[Obj[S]].attr   // Scala does not infer that `obj` must also be `Repr`
-      inAttr.iterator.foreach { case (key, value) =>
-        outAttr.put(key, apply(value))
-      }
-      res
-    case obj: Identifiable[_] =>
-      val id = obj.id.asInstanceOf[S#ID]
-      perform(in, idMap.get(id), idMap.put(id, _))
-    case _ =>
-      perform(in, constMap.get(in), constMap.put(in, _))
+  def apply[Repr <: Elem[S]](in: Repr): Repr = {
+    val out = perform(in)
+    (in, out) match {
+      case (inObj: Obj[S], outObj: Obj[S]) => // copy the attributes
+        val inAttr  = inObj .attr
+        val outAttr = outObj.attr
+        inAttr.iterator.foreach { case (key, value) =>
+          outAttr.put(key, apply(value))
+        }
+      case _ =>
+    }
+    out
   }
 }
