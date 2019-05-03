34f58bd76d98cc75525002ceda22a4068de5e584
Hanns Holger Rutz <contact@sciss.de>
2019-05-03 13:06:55 +0200

    add expr Latch

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 lazy val baseName         = "Lucre"
 lazy val baseNameL        = baseName.toLowerCase
-lazy val projectVersion   = "3.12.0"
+lazy val projectVersion   = "3.12.1-SNAPSHOT"
 lazy val mimaVersion      = "3.12.0"
 
 lazy val deps = new {
---
+++ expr/src/main/scala/de/sciss/lucre/expr/graph/Latch.scala
@@ -0,0 +1,55 @@
+/*
+ *  Latch.scala
+ *  (Lucre)
+ *
+ *  Copyright (c) 2009-2019 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is published under the GNU Lesser General Public License v2.1+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.lucre.expr.graph
+
+import de.sciss.lucre.event.impl.IEventImpl
+import de.sciss.lucre.event.{Caching, IEvent, IPull, ITargets}
+import de.sciss.lucre.expr.{Ex, IExpr, ITrigger, Trig, graph}
+import de.sciss.lucre.stm.Sys
+import de.sciss.lucre.stm.TxnLike.peer
+import de.sciss.model.Change
+
+import scala.concurrent.stm.Ref
+
+object Latch {
+  private final class Expanded[S <: Sys[S], A](init: IExpr[S, A], trig: ITrigger[S], tx0: S#Tx)
+                                              (implicit protected val targets: ITargets[S])
+    extends IExpr[S, A] with IEventImpl[S, Change[A]] with Caching {
+
+    private[this] val ref = Ref(init.value(tx0))
+
+    trig.changed.--->(this)(tx0)
+
+    def value(implicit tx: S#Tx): A = ref()
+
+    def dispose()(implicit tx: S#Tx): Unit =
+      trig.changed.-/->(this)
+
+    def changed: IEvent[S, Change[A]] = this
+
+    private[lucre] def pullUpdate(pull: IPull[S])(implicit tx: S#Tx): Option[Change[A]] = {
+      if (pull(trig.changed).isEmpty) None else {
+        val newValue  = init.value
+        val oldValue  = ref.swap(newValue)
+        if (oldValue == newValue) None else Some(Change(oldValue, newValue))
+      }
+    }
+  }
+}
+final case class Latch[A](in: Ex[A], trig: Trig) extends Ex[A] {
+  def expand[S <: Sys[S]](implicit ctx: Ex.Context[S], tx: S#Tx): IExpr[S, A] = {
+    import ctx.targets
+    new graph.Latch.Expanded(in.expand[S], trig.expand[S], tx)
+  }
+}
