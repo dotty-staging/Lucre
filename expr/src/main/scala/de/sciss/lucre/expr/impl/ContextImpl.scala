920a4b0899e85821addecb6e1f6f3459e37f295d
Hanns Holger Rutz <contact@sciss.de>
2019-08-31 09:37:31 +0200

    intellij find/replace screwed up

--- expr/src/main/scala/de/sciss/lucre/expr/impl/ContextImpl.scala
+++ expr/src/main/scala/de/sciss/lucre/expr/impl/ContextImpl.scala
@@ -5,7 +5,7 @@
  *  Copyright (c) 2009-2019 Hanns Holger Rutz. All rights reserved.
  *
  *  This software is published under the GNU Affero General Public License v3+
-*
+ *
  *
  *  For further information, please contact Hanns Holger Rutz at
  *  contact@sciss.de
