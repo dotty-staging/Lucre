e13a89991814658213e7e5ba2e656ea998e3c1db
Hanns Holger Rutz <contact@sciss.de>
2020-12-05 15:49:59 +0100

    update publish scripts; test ¶ pilcrow for skipping CI build

--- scripts/publishLocal.sh
+++ scripts/publishLocal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sbt +adjunctJS/publishLocal +adjunctJVM/publishLocal +baseJS/publishLocal +baseJVM/publishLocal +confluentJS/publishLocal +confluentJVM/publishLocal +coreJS/publishLocal +coreJVM/publishLocal +dataJS/publishLocal +dataJVM/publishLocal +exprJS/publishLocal +exprJVM/publishLocal +geomJS/publishLocal +geomJVM/publishLocal +lucre-bdb/publishLocal
+sbt +adjunctJS/publishLocal +adjunctJVM/publishLocal +baseJS/publishLocal +baseJVM/publishLocal +confluentJS/publishLocal +confluentJVM/publishLocal +coreJS/publishLocal +coreJVM/publishLocal +dataJS/publishLocal +dataJVM/publishLocal +expr0JS/publishLocal +expr0JVM/publishLocal ++expr1JS/publishLocal +expr1JVM/publishLocal +exprJS/publishLocal +exprJVM/publishLocal +geomJS/publishLocal +geomJVM/publishLocal +lucre-bdb/publishLocal
 
