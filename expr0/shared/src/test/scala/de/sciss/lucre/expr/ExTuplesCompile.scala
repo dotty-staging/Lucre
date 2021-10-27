24a2a5cb881018db4953d3d6c9b1cbc0062f71fc
Hanns Holger Rutz <contact@sciss.de>
2021-10-28 00:26:01 +0200

    some tests for https://github.com/Sciss/SoundProcesses/issues/111 <>

--- expr0/shared/src/test/scala/de/sciss/lucre/expr/ExTuplesCompile.scala
+++ expr0/shared/src/test/scala/de/sciss/lucre/expr/ExTuplesCompile.scala
@@ -1,17 +1,43 @@
 package de.sciss.lucre.expr
 
-import de.sciss.lucre.expr.graph.Ex
+import de.sciss.lucre.expr.graph.{Act, Ex, Trig}
 
 trait ExTuplesCompile {
 //  def runWith(m: ExMap[String, Any]): Unit
+
+  object Attr {
+    import scala.language.implicitConversions
+
+    implicit def fromExTuple[A](tup: A)(implicit view: A => Ex[(String, _)]): Attr = ???
+
+    implicit def fromExAtom[A, B](tup: (A, B))(implicit key: A => Ex[String], value: B => Ex[_]): Attr = ???
+
+    implicit def fromActAtom[A](tup: (A, Act))(implicit key: A => Ex[String]): Attr = ???
+
+    implicit def fromTrigAtom[A](tup: (A, Trig))(implicit key: A => Ex[String]): Attr = ???
+  }
+  trait Attr
 
   def runWith1(m: (Ex[String], Ex[_])*): Unit
 
   def runWith2(m: Ex[(String, _)]*): Unit
 
-  def title: Ex[String]
+  // 27-Oct-2021 : SoundProcesses issue #111
+  def runWith3(m: Attr*): Unit
 
-//  runWith(
+  def value: Ex[String]
+
+  def key: Ex[String]
+
+  def action: Act
+
+  def trigger: Act
+
+  trait Bang extends Act with Trig
+
+  def bang: Bang
+
+  //  runWith(
 //    Map(("foo": Ex[String]) -> ("bar": Ex[String]))
 //  )
 
@@ -19,11 +45,24 @@ trait ExTuplesCompile {
 
   runWith2("foo" -> "bar", "baz" -> 123)
 
+  runWith3("foo" -> "bar", "baz" -> 123)
+
   (1, 2): Ex[(Int, Int)]
 
 //  runWith1("foo" -> title)
 
-  runWith2("foo" -> title)
+  runWith2("foo" -> value)
+
+  runWith3(
+    "key" -> value,
+    key -> value,
+    key -> "value",
+    "callback" -> action,
+    key -> action,
+    "tr" -> trigger,
+    key -> trigger,
+//    "bang" -> bang, // ambiguous
+  )
 
 //  0: Ex[Int]  // ok
 //  Some("foo"): Ex[Option[String]]  // ok
