5e10cb010165d79e0a0e20aae4e616d6f37cb693
Eugene Flesselle <eugene@flesselle.net>
2025-07-17 10:28:41 +0200

    Fix Lucre for #23441 attempt #2.2

--- expr/shared/src/main/scala/de/sciss/lucre/expr/SpanExtensions.scala
+++ expr/shared/src/main/scala/de/sciss/lucre/expr/SpanExtensions.scala
@@ -61,12 +61,11 @@ object SpanExtensions  {
     def readExtension[T <: Txn[T]](opId: Int, in: DataInput, targets: Targets[T])
                                   (implicit tx: T): _Ex[T] = {
       import BinaryOp._
-      val op /* : Op[_, _, _, _] */ = opId /* : @switch */ match {
-        case Apply.id => Apply
-        case Shift.id => Shift
+      opId /* : @switch */ match {
+        case Apply.id => Apply.read[T](in, targets)
+        case Shift.id => Shift.read[T](in, targets)
         case _ => sys.error(s"Unknown extension $opId")
       }
-      op.read[T](in, targets)
     }
   }
 
